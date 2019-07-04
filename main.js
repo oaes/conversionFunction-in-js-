3932c165dd41c129d0f7accf13385ad7739ac6e3
Oaes Kuruni <oaeskuruni27@gmail.com>
2019-07-04 15:07:58 +0600

    added a file

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// .....type of conversation with truely and falsey...
+
+console.log('')
+console.log(0)
+console.log(null)
+console.log(undefined)
+console.log(NaN)
+console.log(Boolean())
+console.log('akash')
+console.log(888)
+console.log(Boolean(44))
+console.log(88+3)
